5177fb3aeb71c53501b8fdf353b580e6ea9d2cff
Benjamin Ihrig <benjamin.ihrig@sap.com>
2019-01-11 11:01:39 +0100

    Do some stuff

--- pi/view.py
+++ pi/view.py
@@ -1,19 +1,73 @@
-import sys
-import time
 import logging
+import sys
+import threading
+import time
+
+import pygame
+from watchdog.events import FileSystemEventHandler
 from watchdog.observers import Observer
-from watchdog.events import LoggingEventHandler
+
+
+class Viewer:
+
+	def __init__(self, fullscreen = True):
+		self._fullscreen = fullscreen
+		pygame.init()
+		displayinfo = pygame.display.Info()
+		self._size = (displayinfo.current_w, displayinfo.current_h)
+		self._window_surface = pygame.display.set_mode(self._size, pygame.FULLSCREEN, 32)
+
+	def run(self):
+		done = False
+		while not done:
+			event = pygame.event.wait()
+			if event.type == pygame.KEYDOWN and event.key == pygame.K_F11:
+				if self._fullscreen:
+					pygame.display.set_mode(self._size)
+					self._fullscreen = not self._fullscreen
+				else:
+					pygame.display.set_mode(self._size, pygame.FULLSCREEN)
+					self._fullscreen = not self._fullscreen
+			if event.type == pygame.QUIT:
+				done = True
+
+		pygame.quit()
+
+
+	def update(self, src_path):
+		try:
+			img = pygame.transform.scale(pygame.image.load(src_path), self._size)
+			self._window_surface.blit(img, (0, 0))
+			pygame.display.flip()
+		except:
+			logging.error("Displaying a new image failed.")
+
+class ViewFileSystemEventHandler(FileSystemEventHandler):
+
+	def __init__(self, viewer):
+		self._viewer = viewer
+
+	def on_created(self, event):
+		self.event(event)
+
+	def on_modified(self, event):
+		self.event(event)
+
+	def event(self, event):
+		if event.is_directory:
+			return
+		self._viewer.update(event.src_path)
+
 
 if __name__ == "__main__":
 	logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 	path = sys.argv[1] if len(sys.argv) > 1 else '.'
-	event_handler = LoggingEventHandler()
+	viewer = Viewer()
+	event_handler = ViewFileSystemEventHandler(viewer)
+
 	observer = Observer()
 	observer.schedule(event_handler, path, recursive=True)
 	observer.start()
-	try:
-		while True:
-			time.sleep(1)
-	except KeyboardInterrupt:
-		observer.stop()
+	viewer.run()
+	observer.stop()
 	observer.join()
